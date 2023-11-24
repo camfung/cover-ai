988744692ec145cce903d72058c187ded154dcd2
Cameron Fung <Cameronfuung@gmail.com>
2023-11-24 02:08:47 -0800

    updated with different configureation for cors

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,16 @@ app.use(
   session({
     secret: config.sessionSecret,
     resave: false,
-    saveUninitialized: true,
-    proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
-    name: "MyCoolWebAppCookieName", // This needs to be unique per-host.
-    cookie: {
-      secure: true, // required for cookies to work on HTTPS
-      httpOnly: false,
-      sameSite: "none",
-    },
+    // saveUninitialized: true,
+    // proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
+    // cookie: {
+    //   secure: true, // required for cookies to work on HTTPS
+    //   httpOnly: false,
+    //   sameSite: "none",
+    // },
   })
 );
-
+app.enable("trust proxy");
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
