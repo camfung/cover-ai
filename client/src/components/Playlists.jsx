00bb65ee84fb704c0b07c841646c0dabac214a41
Cameron Fung <Cameronfuung@gmail.com>
2023-11-26 00:18:09 -0800

    brought grid back

--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -38,35 +38,35 @@ const Playlists = ({ title }) => {
 
             <Grid container spacing={2}>
                 {playlists?.playlists.map((item, index) => (
-                    // <Grid item xs={6} sm={6} md={3} key={index}>
-                    <Card className='card' sx={{ backgroundColor: secondaryColor }} onClick={() => handleCardClick(item.id)}>
-                        <div style={{
-                            width: '100%', // Width of the container
-                            height: 0, // Initial height
-                            paddingBottom: '100%', // Creates a square aspect ratio
-                            position: 'relative', // Enables absolute positioning for the child
-                        }}>
-                            <img
-                                src={item.images[0]?.url}
-                                alt={item.name}
-                                style={{
-                                    position: 'absolute', // Absolute positioning
-                                    top: '50%', // Align center vertically
-                                    left: '50%', // Align center horizontally
-                                    width: 'auto', // Auto width
-                                    height: '100%', // Full height of parent
-                                    transform: 'translate(-50%, -50%)', // Center the image
-                                    objectFit: 'cover', // Cover the area without stretching
-                                }}
-                            />
-                        </div>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" sx={{ color: primaryColor }}>
-                                {item.name}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                    // </Grid>
+                    <Grid item xs={6} sm={6} md={3} key={index}>
+                        <Card className='card' sx={{ backgroundColor: secondaryColor }} onClick={() => handleCardClick(item.id)}>
+                            <div style={{
+                                width: '100%', // Width of the container
+                                height: 0, // Initial height
+                                paddingBottom: '100%', // Creates a square aspect ratio
+                                position: 'relative', // Enables absolute positioning for the child
+                            }}>
+                                <img
+                                    src={item.images[0]?.url}
+                                    alt={item.name}
+                                    style={{
+                                        position: 'absolute', // Absolute positioning
+                                        top: '50%', // Align center vertically
+                                        left: '50%', // Align center horizontally
+                                        width: 'auto', // Auto width
+                                        height: '100%', // Full height of parent
+                                        transform: 'translate(-50%, -50%)', // Center the image
+                                        objectFit: 'cover', // Cover the area without stretching
+                                    }}
+                                />
+                            </div>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div" sx={{ color: primaryColor }}>
+                                    {item.name}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 ))}
             </Grid>
         </>
