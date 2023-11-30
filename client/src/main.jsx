68eb5a614bee3b94a9d43021d27c845fef3b762f
Cameron Fung <Cameronfuung@gmail.com>
2023-11-29 23:06:23 -0800

    removed redux stuff will add back in a pr

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import store from './state/store'
-import { Provider } from 'react-redux'
 const container = document.getElementById('root');
 const root = createRoot(container);
 
