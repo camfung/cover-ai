6a694ef0f1942c3d790f812806a7bdad830089f7
Cameron Fung <Cameronfuung@gmail.com>
2023-12-05 01:18:59 -0800

    added media query for playlist page

--- client/src/components/PlaylistPage.jsx
+++ client/src/components/PlaylistPage.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import BackButton from './buttons/backButton';
 import CreditBar from './dataDisplay/CreditBar';
 import { useCredits } from '../utils/useCredits';
+import useScreenSize from '../hooks/useScreenSize';
 
 const PlaylistPage = () => {
     const [playlistCover, setPlaylistCover] = useState(null);
@@ -19,9 +20,59 @@ const PlaylistPage = () => {
     const [errorOpen, setErrorOpen] = useState(false);
     const { credits, fetchCredits } = useCredits();
     const [outOfCreditsOpen, setOutOfCreditsOpen] = useState(false);
-    console.log(location.state)
+    const screenSize = useScreenSize();
+    const boxStyle = {
+        p: 3,
+        height: screenSize != "small" ? "80vh" : "calc(100vh - 70px) ",
+        maxWidth: "1024px",
+        display: 'flex',
+        flexDirection: "column",
+        margin: "0 auto",
+    };
 
+    const innerBoxStyle = {
+        height: "80%",
+        display: screenSize != "small" ? "grid" : "flex",
+        gridTemplateColumns: "2fr 3fr",
+        alignItems: 'center',
+        flexDirection: "column",
+        gridGap: "10% 10%"
+    };
 
+    const songBoxStyle = {
+        height: screenSize != "small" ? "90%" : "30%",
+        overflow: "hidden",
+        flexGrow: 2,
+    };
+
+    const selectedSongsTypographyStyle = {
+        mb: 2,
+        backgroundColor: "#FF7700",
+        color: "white"
+    };
+
+    const coverBoxStyle = {
+        border: "3px dashed black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: screenSize != "small" ? "90%" : "auto",
+        aspectRatio: "1 / 1",
+        flexGrow: 3,
+    };
+
+    const buttonContainerStyle = {
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: screenSize != "small" ? "2fr 3fr" : "1fr 1fr",
+        gridGap: "10% 10%",
+        marginTop: screenSize != "small" ? "auto" : "20px"
+    };
+
+    const buttonStyle = {
+        maxWidth: "300px",
+        margin: "0 auto"
+    };
     const handleConfirm = useCallback(async () => {
         try {
             const selectedSongIds = selectedSongs.map(song => song.track.id).join(",");
@@ -61,14 +112,15 @@ const PlaylistPage = () => {
         navigate("/playlist/" + playlistId + "/" + location.state.playlistTitle)
     }, [])
     return (
-        <Box sx={{ p: 3, height: "80vh", maxWidth: "1024px", display: 'flex', flexDirection: "column", margin: "0 auto" }}>
+        <Box sx={boxStyle}>
             <CreditBar credits={credits} goBack={goBack} playlistTitle={decodeURIComponent(location.state.playlistTitle)} />
 
-            {import.meta.env.VITE_NODE_ENV == "local" ? <Switch checked={debug} onChange={(event) => handleSwitch(event)}>Free Image</Switch> : <></>}
-            <Box sx={{ height: "80%", display: "grid", gridTemplateColumns: "2fr 3fr", alignItems: 'center', justifyContent: "center", gridGap: "10% 10%", }}>
-
-                <Box sx={{ backgroundColor: "white", height: "90%", overflow: "hidden" }}>
-                    <Typography variant="h6" sx={{ mb: 2, backgroundColor: "#FF7700", color: "white" }}>
+            {/* {import.meta.env.VITE_NODE_ENV === "local" && (
+                <Switch checked={debug} onChange={(event) => handleSwitch(event)}>Free Image</Switch>
+            )} */}
+            <Box sx={innerBoxStyle}>
+                <Box sx={songBoxStyle}>
+                    <Typography variant="h6" sx={selectedSongsTypographyStyle}>
                         Selected Songs
                     </Typography>
                     <Paper sx={{ overflow: "auto", height: "100%" }}>
@@ -81,21 +133,12 @@ const PlaylistPage = () => {
                         </List>
                     </Paper>
                 </Box>
-                <Box sx={{
-                    // Set the width equal to the height to make the box a square
-                    border: "3px dashed black",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    height: "90%",
-                    aspectRatio: "1 / 1",
-                }}>
+                <Box sx={coverBoxStyle}>
                     {playlistCover ? (
-                        <>
-                            {playlistCover}
-
-                        </>
-                    ) : <Typography>Playlist cover will appear here.</Typography>}
+                        <>{playlistCover}</>
+                    ) : (
+                        <Typography>Playlist cover will appear here.</Typography>
+                    )}
                     {loading && (
                         <>
                             <Typography>Generating Image</Typography>
@@ -104,18 +147,18 @@ const PlaylistPage = () => {
                     )}
                 </Box>
             </Box>
-            <Box sx={{ width: "100%", display: "grid", gridTemplateColumns: "2fr 3fr", gridGap: "10% 10%", }}>
+            <Box sx={buttonContainerStyle}>
                 <Button
                     variant="contained"
                     color="primary"
                     onClick={handleConfirm}
-                    sx={{ maxWidth: "300px", margin: "0 auto" }}
+                    sx={buttonStyle}
                 >
-                    Confirm and Generate Cover
+                    Generate Cover
                 </Button>
                 <Button
                     variant="contained"
-                    sx={{ maxWidth: "300px", margin: "0 auto" }}
+                    sx={buttonStyle}
                     color="primary"
                     onClick={sendImageToSpotify}
                 >
